6dcaedeaa33ce4bece822e3571a08df4b6c5087d
Satish <satish.a.4u@gmail.com>
2021-08-21 19:30:54 +0530

    Fixed cascaded dropdowns

--- PracticeApp/PracticeApp/Controllers/AccountController.cs
+++ PracticeApp/PracticeApp/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using PracticeApp.Models;
 using System;
 using System.Collections.Generic;
@@ -9,9 +10,33 @@ namespace PracticeApp.Controllers
 {
     public class AccountController : Controller
     {
+        public List<City> lstCities = new List<City>()
+        {
+            new City() { CityId = 1, CityName = "Hyderabad", CountryId = 2 },
+            new City() { CityId = 2, CityName = "Chennai", CountryId = 2 },
+            new City() { CityId = 3, CityName = "New York", CountryId = 1 },
+            new City() { CityId = 4, CityName = "California", CountryId = 1 },
+
+        };
+
+
+        public List<Country> lstCountires = new List<Country>()
+        {
+            new Country() { CountryId = 1, CountryName = "United States of America" },
+            new Country() { CountryId = 2, CountryName = "India" }
+
+
+        };
         public IActionResult SignUp()
         {
+            ViewBag.CountryList = new SelectList(lstCountires, "CountryId", "CountryName");
             return View();
+        }
+
+        public JsonResult GetCityList(int CountryId)
+        {
+            List<City> filterCityList = lstCities.FindAll(m => m.CountryId == CountryId);
+            return new JsonResult(filterCityList);
         }
 
         [HttpPost]
--- PracticeApp/PracticeApp/Models/UserModel.cs
+++ PracticeApp/PracticeApp/Models/UserModel.cs
@@ -39,4 +39,19 @@ namespace PracticeApp.Models
         public bool Terms{ get; set; }
     }
 
+    public class Country
+    {
+        public int CountryId { get; set; }
+        public string CountryName { get; set; }
+    }
+
+    public class City
+    {
+
+        public int CityId { get; set; }
+        public string CityName { get; set; }
+        public int CountryId { get; set; }
+
+    }
+
 }
